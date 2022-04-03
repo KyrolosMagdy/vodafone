b1e1fef16c15d67a15c0152422e70e4789c6198a
kryolos.hakeem <kryolos.hakeem@warbabank.com>
2022-04-03 19:29:06 +0200

    adding the skelton, installing redux, material-ui

---
+++ src/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = (): React.ReactElement => {
+    return (
+        <div>
+            <h2>
+                Hey this is our main screen for this project
+            </h2>
+        </div>
+    )
+};
+
+export default MainPage;
